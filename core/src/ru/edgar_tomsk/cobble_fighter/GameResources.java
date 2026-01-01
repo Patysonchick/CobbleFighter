c6751d9120920e5f40d40248d72f5b8bf0e0921b
Эдгар Томский <edgarkozlov3229@gmail.com>
2026-01-01 19:28:01 +0300

    Added background

--- core/src/ru/edgar_tomsk/cobble_fighter/GameResources.java
+++ core/src/ru/edgar_tomsk/cobble_fighter/GameResources.java
@@ -3,4 +3,5 @@ package ru.edgar_tomsk.cobble_fighter;
 public class GameResources {
     public static final String GLOVE_IMG_PATH = "textures/glove.png";
     public static final String COBBLE_IMG_PATH = "textures/cobble.png";
+    public static final String BACKGROUND_IMG_PATH = "textures/background.jpg";
 }
--- core/src/ru/edgar_tomsk/cobble_fighter/screens/GameScreen.java
+++ core/src/ru/edgar_tomsk/cobble_fighter/screens/GameScreen.java
@@ -3,6 +3,7 @@ package ru.edgar_tomsk.cobble_fighter.screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.ScreenUtils;
 
@@ -20,14 +21,15 @@ public class GameScreen extends ScreenAdapter {
     CobbleFighter CobbleFighter;
     GameSession gameSession;
     GloveObject gloveObject;
-
+    Texture backgroundTexture;
     ArrayList<CobbleObject> cobbleArray;
 
     public GameScreen(CobbleFighter CobbleFighter) {
         this.CobbleFighter = CobbleFighter;
         gameSession = new GameSession();
-
         cobbleArray = new ArrayList<>();
+
+        backgroundTexture = new Texture(GameResources.BACKGROUND_IMG_PATH);
 
         gloveObject = new GloveObject(
                 GameSettings.SCREEN_WIDTH / 2, 150,
@@ -71,16 +73,17 @@ public class GameScreen extends ScreenAdapter {
     }
 
     private void draw() {
-
         CobbleFighter.camera.update();
         CobbleFighter.batch.setProjectionMatrix(CobbleFighter.camera.combined);
         ScreenUtils.clear(Color.CLEAR);
 
         CobbleFighter.batch.begin();
+
+        CobbleFighter.batch.draw(backgroundTexture, 0, 0, GameSettings.SCREEN_WIDTH, GameSettings.SCREEN_HEIGHT);
+
         for (CobbleObject cobble : cobbleArray) cobble.draw(CobbleFighter.batch);
         gloveObject.draw(CobbleFighter.batch);
         CobbleFighter.batch.end();
-
     }
 
     private void updateCobble() {
